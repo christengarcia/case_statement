5ec905d0d324bf1ee64b13b123caa1c81b477d41
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 20:54:54 +1100

    First commit

---
+++ case_statement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace case_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade: ");
+        }
+    }
+}
