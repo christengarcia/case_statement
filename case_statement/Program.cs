ed3599254fb2ae7424ca450118565a11fad8814c
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 20:59:48 +1100

    Added case statements for grades.

--- case_statement/Program.cs
+++ case_statement/Program.cs
@@ -7,6 +7,24 @@ namespace case_statement
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your grade: ");
+            string userGrade = Console.ReadLine();
+
+            switch (userGrade)
+            {
+                case "A+":
+                case "A":
+                    Console.WriteLine("Distinction");
+                    break;
+                case "B":
+                    Console.WriteLine("B Grade");
+                    break;
+                case "C":
+                    Console.WriteLine("C Grade");
+                    break;
+                default:
+                    Console.WriteLine("Fail");
+                    break;
+            }
         }
     }
 }
